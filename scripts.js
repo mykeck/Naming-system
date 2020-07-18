90d9187283dac0a6c8722f5e788cada6abecf08e
MikeC <kiragumike@gmail.com>
2020-07-18 19:35:06 +0300

    styled forms

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 var maleNames = ["Kwasi", "Kwandwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-Var dayOfTheWeek = ["Sunday", "Monday", "Teusday", "Wednesday", "Thursday", "friday","Saturday"];
+var dayOfTheWeek = ["Sunday", "Monday", "Teusday", "Wednesday", "Thursday", "friday","Saturday"];
 
 function AkanName () {
     // var year = parseInt(document.getElementById("year");
