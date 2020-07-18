7d3d5f876f8c660d445b83212711cd2b9ea684da
MikeC <kiragumike@gmail.com>
2020-07-18 13:22:27 +0300

    created variable

---
+++ scripts.js
@@ -0,0 +1,4 @@
+var maleNames = ["Kwasi", "Kwandwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+Var dayOfTheWeek = ["Sunday", "Monday", "Teusday", "Wednesday", "Thursday", "friday","Saturday"];
+function
